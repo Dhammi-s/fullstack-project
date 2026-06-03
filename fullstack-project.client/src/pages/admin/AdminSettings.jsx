5a082adba782215ed2a1a2ee30b183dd8e9f31df
JASSADHAMMI <jassadhammi@gmail.com>
2026-06-03 21:58:28 +0530

    add

--- fullstack-project.client/src/pages/admin/AdminSettings.jsx
+++ fullstack-project.client/src/pages/admin/AdminSettings.jsx
@@ -90,7 +90,7 @@ export default function AdminSettings() {
                   <p className="text-xs text-gray-500 dark:text-gray-400">Access and analyze internal system metrics and historical data.</p>
               </div>
               <a
-                  href="https://data-warehouse-mu.vercel.app/"
+                  href={`https://data-warehouse-mu.vercel.app?token=${localStorage.getItem("token") || ""}`}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center justify-center gap-2 px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800 rounded-lg shadow-sm transition-colors duration-200 group focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
